6a0a11ef93ddbfdee13d759800e624437e1e93c9
Vova <stemart@ukr.net>
2017-05-08 23:46:03 +0000

    p

--- index.js
+++
@@ -1,9 +0,0 @@
-function setImgPos(img){
-    var boxWidth = 920;
-    var boxHeight = 920;
-     
-    if(img.width < boxWidth)
-        img.style.marginLeft = ((boxWidth - img.width) / 2) + 'px';
-    if(img.height < boxHeight)
-        img.style.marginTop = ((boxHeight - img.height) / 2) + 'px';
-}
